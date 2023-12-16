729743ee445cd9069f65801ecd718adaed127050
Shakil Khan <m.shakilkhan720@gmail.com>
2023-12-16 18:45:52 +0600

    circle_with_inside_and_border_color done

---
+++ Class-1/circle_with_inside_and_border_color.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+#Border and fill Color
+pencolor('red')
+fillcolor('blue')
+
+# Draw a circle and fill with color
+begin_fill()
+circle(80)
+end_fill()
+
+hideturtle()
+exitonclick()
